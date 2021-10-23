ed262de8dc98f21e2aaae1e3edd06c0c0aee5387
Snerzel <Snerzel@comast.net>
2021-10-23 12:02:33 -0700

    migration successfull

--- db/migrate/20211021222714_create_inspirational_quotes.rb
+++ db/migrate/20211021222714_create_inspirational_quotes.rb
@@ -1,4 +1,4 @@
-class CreateInspirationalQuotes < ActiveRecord::Migration
+class CreateInspirationalQuotes < ActiveRecord::Migration[5.2]
   def change
     create_table :inspirational_quotes do |t|
       t.string :content
