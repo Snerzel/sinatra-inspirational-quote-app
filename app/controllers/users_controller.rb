5a5b2b829fbe0c451d76722419e882d94381833b
Snerzel <Snerzel@comast.net>
2021-10-24 10:56:11 -0700

    get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,22 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
-        
+    @user = User.find_by(email: params[:email])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            puts session
+            redirect "users/#{@user.id}"
+            else
+
+        end
     end
 
     get '/signup' do
 
+    end
+
+    get '/users/:id' do
+        "show"
     end
 
 end
