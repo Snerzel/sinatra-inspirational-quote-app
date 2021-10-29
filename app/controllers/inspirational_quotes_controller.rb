13b794bb71ec7d703ca48646679454596ae3773c
Snerzel <Snerzel@comast.net>
2021-10-28 19:53:07 -0700

    adds get and post request for inspirational_quotes_controller

--- app/controllers/inspirational_quotes_controller.rb
+++ app/controllers/inspirational_quotes_controller.rb
@@ -4,6 +4,21 @@ class InspirationalQuotesController < ApplicationController
         erb :'/inspirational_quotes/new'
     end
 
+    post '/inspirational_quotes' do
+        if !logged_in?
+            redirect '/'
+        end
 
+        if params[:content] != ""
+            @inspirational_quote = InspirationalQuote.create(content: params[:content], user_id: current_user.id)
+            redirect "/inspirational_quotes/#{@inspirational_quote.id}"
+        else
+            redirect '/inspirational_quotes/new'
+        end
+    end
 
+    get '/inspirational_quotes/:id'
+        @inspirational_quote = InspirationalQuote.find(params[:id])
+        erb :'/inspiration_quotes/show'
+    end
 end
